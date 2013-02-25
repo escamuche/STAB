ad746c5b768b5cffbc9e056baf5826652dea5472
Carlos Núñez <escamuche@gmail.com>
2013-02-25 18:58:03 +0100

    ejemplo de buff

--- stab/src/com/stab/data/actions/player/DefendAction.java
+++ stab/src/com/stab/data/actions/player/DefendAction.java
@@ -4,6 +4,8 @@ import com.stab.data.animation.DefendAnimation;
 import com.stab.model.action.SelfAction;
 import com.stab.model.events.common.ConsoleMessage;
 import com.stab.model.info.BaseInfo;
+import com.stab.model.info.trait.Effect;
+import com.stab.model.info.trait.Modifier;
 import com.stab.model.request.basic.ActionRequest;
 
 public class DefendAction extends SelfAction {
@@ -20,6 +22,12 @@ public class DefendAction extends SelfAction {
 	public boolean execute(BaseInfo self) {
 		self.playAnimation(DefendAnimation.ID);
 		self.getScene().sendMessage(ConsoleMessage.SUCCESS, self.getText()+" defends");
+		
+		Modifier masdosalaCA= Modifier.createMod("CA", +2);  // Creamos un modificador de +2 a la ca
+		Effect buffqueduraunround = Effect.createRound("Nombre innecesario de ejemplo", masdosalaCA); //creamos un efecto que dura 1 round, y que aplica ese modificador (y lo quita cuando se pasa)
+		
+		self.addTrait(buffqueduraunround); //nos añadimos el buff
+		
 		return true;
 	}
 	
