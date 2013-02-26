cda8d5acd27afecb5c80503743e624f8e0277369
Ferrus <malacor@gmail.com>
2013-02-26 18:28:02 +0100

    confirmaciones criticos/pifias

--- stab/src/com/stab/data/actions/player/Kill.java
+++ stab/src/com/stab/data/actions/player/Kill.java
@@ -23,46 +23,65 @@ public class Kill extends TargetAction{
 		BaseInfo elqueMata = (BaseInfo)yo;
 		
 		int ac = aMatar.getValue(StabConstants.ARMOR);
-		int dado = Roll.d20();
+		int dado = Roll.d20()+1;
 		int dañobase=elqueMata.getValue(StabConstants.DAMAGE);
+		System.out.println(dado + " en el dado!");
 		
 		
-		if (dado == 20) {  
+		if (dado == 20) {
+			System.out.println("Posibilidad de Critico!");
+			int confir=Roll.d20();
+			int caconfir=elqueMata.getValue(StabConstants.TOHIT) + confir;
+			if(ac <= caconfir) {
 			dañobase=dañobase*2;
 		    critico=true;
+		    System.out.println("Critico!!!");
+			   }
+			else{
+				System.out.println("Fallaste la confirmacion!");
+			}
 			}
 		
 		if (dado == 1) {
+			System.out.println("Posibilidad de Pifia!");
+		    int confir=Roll.d20();
+		    int caconfir=elqueMata.getValue(StabConstants.TOHIT) + confir;
+		    if(ac <= caconfir) {
+	        System.out.println("Casi!");
+		   }
+		else{
+			System.out.println("Pifia!!!");
 			pifia=true;
 		    }
 		
 		int dar = elqueMata.getValue(StabConstants.TOHIT) + dado;
-		elqueMata.getScene().sendMessage(ConsoleMessage.SUCCESS, "Impactas a armadura " + dar);
+		System.out.println("Impactas a armadura " + dar);
 	
 		if (ac <= dar) { //si das a ca
 			
 			if (pifia == true) { //pero es pifia
-				elqueMata.getScene().sendMessage(ConsoleMessage.ERROR, "Fallas el golpe");
+				System.out.println("Fallas el golpe");
 				return false;				
 							}
 		           //das y no es pifia
 						Damage d= new Damage(elqueMata.getValue(StabConstants.DAMAGE), Damage.SLASHING_DAMAGE,yo);
 						aMatar.apply(d);
-						elqueMata.getScene().sendMessage(ConsoleMessage.WARNING, d.getAmount()+" de daño");
+						System.out.println(d.getAmount()+" de daño");
 						return true;	
 		}
 			else { // si no das a ca
 				if (critico == true) { //pero es critico
 					Damage d= new Damage(elqueMata.getValue(StabConstants.DAMAGE), Damage.SLASHING_DAMAGE,yo);
 					aMatar.apply(d);
-					elqueMata.getScene().sendMessage(ConsoleMessage.WARNING, d.getAmount()+" de daño");
+					System.out.println(d.getAmount()+" de daño");
 					return true;	
 					}
 				 //no das y no es critico
-				elqueMata.getScene().sendMessage(ConsoleMessage.ERROR, "Fallas el golpe");
+				System.out.println("Fallas el golpe");
 				return false;	
 		    }
-			
+		}
+		return critico;
 		}
 	
 	public Kill() {
