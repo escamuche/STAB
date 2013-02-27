7ad25cccb477e086d89d08b3ccbf228aedc2b122
Carlos Núñez <escamuche@gmail.com>
2013-02-27 15:29:38 +0100

    arreglado lever primer intento de attack como applicable

---
+++ stab/src/com/stab/data/info/applicable/Attack.java
@@ -0,0 +1,92 @@
+package com.stab.data.info.applicable;
+
+import com.stab.data.StabConstants;
+import com.stab.model.info.BaseInfo;
+import com.stab.model.info.applicable.Applicable;
+import com.stab.util.Roll;
+
+public class Attack extends Applicable{
+
+	
+	public static final int HIT = 0;
+	public static final int MISS = 10;
+	public static final int CRITICAL= 5;
+	public static final int BOTCH = 11;
+	
+	
+	public Attack(BaseInfo instigator) {
+		super(instigator);
+	}
+
+
+	@Override
+	public void apply() {
+		BaseInfo aMatar = getTarget();
+		BaseInfo elqueMata = (BaseInfo) getInstigator();
+		
+		int ac = aMatar.getValue(StabConstants.ARMOR);
+		int dado = Roll.d20();
+		int dañobase=elqueMata.getValue(StabConstants.DAMAGE);
+		int hit=elqueMata.getValue(StabConstants.TOHIT);
+		System.out.println(dado + " en el dado!");
+		
+		
+		if (dado == 20) {
+			System.out.println("Posibilidad de Critico!");
+			int confirc=Roll.d20();
+			int caconfirc=hit + confirc;
+			if(ac <= caconfirc) {
+				dañobase=dañobase*2;
+				System.out.println("Critico!!!");
+				setResult(CRITICAL);
+			   	}
+			else{
+				System.out.println("Fallaste la confirmacion!");
+				setResult(HIT);
+				}
+			}
+		
+		
+		if (dado == 1) {
+			System.out.println("Posibilidad de Pifia!");
+		    int confirp=Roll.d20();
+		    int caconfirp=hit + confirp;
+		    if(ac <= caconfirp) {
+		    	System.out.println("Casi!");
+		    	setResult(MISS);
+		    	}
+		    	else{
+		    		System.out.println("Pifia!!!");
+		    		setResult(BOTCH);
+		    		}
+			}
+		
+		
+		int dar = elqueMata.getValue(StabConstants.TOHIT) + dado;
+		System.out.println("Impactas a armadura " + dar);
+	
+		if (ac <= dar) { //si das a ca
+			
+			setResult(MISS);
+		}
+		else{
+			setResult(HIT);
+		}
+					
+	}
+	
+	public boolean hits(){
+		return getResult()==HIT || getResult()==CRITICAL;
+	}
+	
+	public boolean misses(){
+		return !hits();
+	}
+	
+	public boolean isCritical(){
+		return getResult()==CRITICAL;
+	}
+	public boolean isBotch(){
+		return getResult()==BOTCH;
+	}
+}
