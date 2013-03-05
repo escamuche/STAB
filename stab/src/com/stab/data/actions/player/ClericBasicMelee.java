a103fa4373216aa61044c7bd17afa140ea84319c
Ferrus <malacor@gmail.com>
2013-03-05 10:51:02 +0100

    loquesigue

---
+++ stab/src/com/stab/data/actions/player/ClericBasicMelee.java
@@ -0,0 +1,25 @@
+package com.stab.data.actions.player;
+
+import com.stab.data.StabConstants;
+import com.stab.data.actions.player.Kill;
+import com.stab.model.info.BaseInfo;
+import com.stab.util.Roll;
+
+public class ClericBasicMelee extends Kill {
+	
+	public static final String ID="CLERIC_ATTACK";
+	
+	protected String getSwingImage(BaseInfo atacante) {
+		return "effects/ClericAttack";
+	}
+	
+	public ClericBasicMelee() {
+		setResource("actions/baston");
+	}
+	@Override
+	protected int getBaseDamage(BaseInfo Atacante) {
+		
+		return Roll.d8()+Atacante.getAttributeValue(StabConstants.DAMAGE);
+	}
+
+}
