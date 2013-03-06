99744c489673ad0ff2dcd00b8a07a8d2caa03431
Carlos Núñez <escamuche@gmail.com>
2013-03-06 09:44:36 +0100

    clases basicas para spells

--- stab/src/com/stab/data/actions/player/spells/Spell.java
+++ stab/src/com/stab/data/actions/player/spells/Spell.java
@@ -1,8 +1,12 @@
 package com.stab.data.actions.player.spells;
 
+import com.stab.model.info.BaseInfo;
+
 public interface Spell {
   
 	
 	public int getLevel();
+	public String getAttribute();
+	public int getDC(BaseInfo caster);
 	
 }
--- stab/src/com/stab/data/actions/player/spells/SpellOnTile.java
+++ stab/src/com/stab/data/actions/player/spells/SpellOnTile.java
@@ -1,16 +1,42 @@
 package com.stab.data.actions.player.spells;
 
 import com.stab.model.action.TileAction;
+import com.stab.model.info.BaseInfo;
 
 public abstract class SpellOnTile extends TileAction  implements Spell {
 
 	int level=0;
+	String attribute;
 	
 	public void setLevel(int level) {
 		this.level = level;
 	}
 	public int getLevel() {
 		return level;
+	}
+	
+	
+	public void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}
+	@Override
+	public String getAttribute() {
+		return attribute;
+	}
+	
+	@Override
+	public int getDC(BaseInfo caster) {
+		int i=10+getLevel();
+		i=i+(caster.getValue(getAttribute())-10)/2;
+		//Calcular bonos por escuela en base a feats
+		return i;
+	}
+	
+	@Override
+	public boolean validateActor(BaseInfo i) {
+		if (i.getValue(getAttribute())<10+getLevel())
+			return false;
+		return super.validateActor(i);
 	}
 	
 }
