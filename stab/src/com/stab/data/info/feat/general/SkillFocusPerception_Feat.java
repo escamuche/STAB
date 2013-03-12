a32c473a0dbb29ef8da5ea80ce60ef019ea91471
Ferrus <malacor@gmail.com>
2013-03-12 13:09:50 +0100

    cambios skill focus perception

--- stab/src/com/stab/data/info/feat/general/SkillFocusPerception_Feat.java
+++ stab/src/com/stab/data/info/feat/general/SkillFocusPerception_Feat.java
@@ -1,8 +1,9 @@
 package com.stab.data.info.feat.general;
 
-import com.stab.data.actions.feats.SkillFocusPerception;
+import com.stab.data.StabConstants;
 import com.stab.data.info.feat.CombatFeat;
 import com.stab.model.info.BaseInfo;
+import com.stab.model.info.trait.Modifier;
 
 public class SkillFocusPerception_Feat extends CombatFeat{
 	
@@ -11,19 +12,25 @@ public class SkillFocusPerception_Feat extends CombatFeat{
 	
 	
 	public SkillFocusPerception_Feat() {
+			
 		
-	}
+			Modifier buff=Modifier.createMod(StabConstants.PERCEPTION,+3);
+					
+			addTrait(buff);
+			
+			}
+		
 	
 	@Override
 	public void applyTo(BaseInfo baseInfo) {
 		super.applyTo(baseInfo);
-		grantAction(baseInfo, SkillFocusPerception.ID);
+		grantAction(baseInfo, SkillFocusPerception_Feat.ID);
 	}
 	
 	@Override
 	public void removeFrom(BaseInfo baseInfo) {
 		super.removeFrom(baseInfo);
-		removeAction(baseInfo, SkillFocusPerception.ID);
+		removeAction(baseInfo, SkillFocusPerception_Feat.ID);
 	}
 	
 }
