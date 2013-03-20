2aebc0fd1ff5b34a2b80d8b4c3890aaf4bceb108
Carlos Núñez <escamuche@gmail.com>
2013-03-20 10:27:09 +0100

    atajos equip

--- stab/src/com/stab/data/info/monster/Monster.java
+++ stab/src/com/stab/data/info/monster/Monster.java
@@ -1,9 +1,11 @@
 package com.stab.data.info.monster;
 
 import com.stab.data.StabConstants;
+import com.stab.data.StabInit;
 import com.stab.data.info.BasicAttributes;
 import com.stab.data.info.equipment.HumanoidGear;
 import com.stab.model.info.base.Creature;
+import com.stab.model.info.trait.base.Equipment;
 import com.stab.util.Roll;
 
 public class Monster extends Creature {
@@ -22,6 +24,19 @@ public class Monster extends Creature {
 	@Override
 	public void rollInitiative() {
 		setInitiative(Roll.d20()+getValue(StabConstants.INICIATIVEMOD));
+	}
+	
+	
+
+	public boolean equip(String s) {
+		Equipment e= StabInit.getWeaponFactory().getWeapon(s);
+		if (e==null)
+			e=StabInit.getArmorFactory().getArmor(s);
+		//if (e==null)
+			//e=StabInit.getEquipmentFactory().getEquipment(s);
+		if (e==null)
+			return false;
+		return super.equip(e);
 	}
 	
 }
