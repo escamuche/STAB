4280ed0dccae6188ee38fbfb7eda3656191fad50
Carlos Núñez <escamuche@gmail.com>
2013-04-02 16:08:05 +0200

    half bonus modifier

---
+++ stab/src/com/stab/data/info/HalfBonusModifier.java
@@ -0,0 +1,21 @@
+package com.stab.data.info;
+
+import com.stab.model.info.trait.AttributeBasedModifier;
+
+public class HalfBonusModifier extends AttributeBasedModifier {
+	
+	public HalfBonusModifier(String target, String from) {
+		this(target,from,false);
+	}
+	
+	public HalfBonusModifier(String target, String from,boolean useBaseValue) {
+		super(target,from,useBaseValue);
+	}
+	
+	
+	@Override
+	public int getValue() {
+		return (int)((super.getValue()+1)/2);
+	}
+
+}
