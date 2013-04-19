7161888840d0a6e941178bdaf1172f997c17a873
Ferrus <malacor@gmail.com>
2013-04-19 14:47:59 +0200

    illusion_buff

--- stab/src/com/stab/data/info/buff/Illusion_Buff.java
+++ stab/src/com/stab/data/info/buff/Illusion_Buff.java
@@ -1,22 +1,27 @@
 package com.stab.data.info.buff;
 
+import com.stab.data.StabConstants;
 import com.stab.data.info.applicable.Attack;
-import com.stab.data.info.applicable.magic.WillAttack;
+import com.stab.data.info.applicable.SavingThrowEffect;
 import com.stab.model.info.BaseInfo;
 import com.stab.model.info.applicable.Affects;
 import com.stab.model.info.applicable.Applicable;
+import com.stab.model.info.applicable.Attends;
 import com.stab.model.info.trait.base.Buff;
 
-public class Illusion_Buff extends Buff implements Affects<Attack> {
+public class Illusion_Buff extends Buff implements Affects<Attack>, Attends<Attack> {
 
 	@Override
 	public void affect(Attack arg0) {
 		
 		BaseInfo target=getTarget();
-		WillAttack d = new WillAttack(target);
-		target.apply(d);
-		if(d.hits()){
-			
+		BaseInfo instigator = (BaseInfo) arg0.getInstigator();
+		SavingThrowEffect d = new SavingThrowEffect(instigator,StabConstants.WILLSAVE,target);
+		d.setTargetNumber(16);
+		d.check();
+		if(d.success()){
+			arg0.setResult(Attack.FAIL);
+			instigator.destroy();
 		}
 		else{
 			
@@ -28,7 +33,32 @@ public class Illusion_Buff extends Buff implements Affects<Attack> {
 	@Override
 	public boolean canAffect(Applicable arg0) {
 		// TODO Auto-generated method stub
-		return false;
+		return true;
+	}
+
+	@Override
+	public void attend(Attack arg0) {
+		
+		BaseInfo target = arg0.getTarget();
+		BaseInfo instigator = (BaseInfo) arg0.getInstigator();
+		SavingThrowEffect d = new SavingThrowEffect(target, StabConstants.WILLSAVE, instigator);
+		d.setTargetNumber(16);
+		d.check();
+		if(d.success()){
+			instigator.destroy();
+		}
+		
+		else {
+			
+		}
+		
+		
+	}
+
+	@Override
+	public boolean canAttend(Applicable arg0) {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 }
