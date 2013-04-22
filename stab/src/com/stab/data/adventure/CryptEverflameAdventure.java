749d10f739c5af68e8e85bf5abfdb53f4187103f
Ferrus <malacor@gmail.com>
2013-04-22 14:03:49 +0200

    flame#7

--- stab/src/com/stab/data/adventure/CryptEverflameAdventure.java
+++ stab/src/com/stab/data/adventure/CryptEverflameAdventure.java
@@ -40,6 +40,8 @@ public class CryptEverflameAdventure extends Adventure{
 		super.init();
 		clearScenes();
 		
+	//escena 1
+		
 		Narration n1=new Narration();
 		n1.createContents();
 		n1.setTag("BEGIN");
@@ -59,21 +61,23 @@ public class CryptEverflameAdventure extends Adventure{
 		t1.setPos(Constants.BEGIN, Constants.BEGIN);
 		t1.setSize(Constants.CONTENT,Constants.CONTENT);
 		
-		Image i = new Image();
-		i.setImage("kassen");
-		i.setPos(Constants.BEGIN, Constants.NEXT);
+		Image i1 = new Image();
+		i1.setImage("nirmathas$S");
+		i1.setPos(Constants.BEGIN, Constants.NEXT);
+		i1.setSize(Constants.FILL, Constants.FILL);
 		
 		n1.addGUI(t1);
-		n1.addGUI(i);
+		//n1.addGUI(i1);
 		this.addScene(n1);
+		
+	//escena 2
 		
 		Narration n2=new Narration();
 		n2.createContents();
 		n2.setTag("INTRO");
 		n2.setNext("INTRO2");
 		n2.setBackground("COLOR#orange");
-		Image i2 = new Image();
-		i2.setImage("kassen");
+		
 		Text t2=new Text();
 		t2.setText("Ekat Kassen era un cruzado y buscador de fortuna que llego " +
 				"a servir a Lastwall en el año 4515 AR. Aunque lucho con distincion, " +
@@ -93,18 +97,25 @@ public class CryptEverflameAdventure extends Adventure{
 				"los lugareños encontraron el campamento de los mercenarios en una antigua cripta en lo " +
 				"profundo del bosque, y el mismo Ekat fue a lidiar con su antiguo compañero.");
 		t2.setSize(Constants.CONTENT,Constants.CONTENT);
+		t2.setPos(Constants.BEGIN, Constants.BEGIN);
+		
+		Image i2 = new Image();
+		i2.setImage("nirmathasmap$S");
 		i2.setPos(Constants.BEGIN, Constants.NEXT);
-		n2.addGUI(i2);
+		i2.setSize(Constants.CONTENT, Constants.CONTENT);
+		
 		n2.addGUI(t2);
+		n2.addGUI(i2);
 		this.addScene(n2);
+		
+	//escena 3
 		
 		Narration n3=new Narration();
 		n3.createContents();
 		n3.setTag("INTRO2");
 		n3.setNext("INTRO3");
 		n3.setBackground("COLOR#orange");
-		Image i3 = new Image();
-		i3.setImage("kassen");
+		
 		Text t3 = new Text();
 		t3.setText("La batalla fue terriblemente sangrienta, solo unos pocos de la gente de la ciudad volvio " +
 				"para contar lo sucedido. Al final Asar murio y sus mercenarios fueron derrotados, pero " +
@@ -124,18 +135,25 @@ public class CryptEverflameAdventure extends Adventure{
 				"Cuando los aventureros retornan unos pocos dias despues, la ciudad celebra una gran fiesta en su honor. " +
 				"Esto tambien marca la ultima celebracion antes de que comience el duro invierno. ");
 		t3.setSize(Constants.CONTENT,Constants.CONTENT);
+		t3.setPos(Constants.BEGIN, Constants.BEGIN);
+		
+		Image i3 = new Image();
+		i3.setImage("kassen$S");
 		i3.setPos(Constants.BEGIN, Constants.NEXT);
-		n3.addGUI(i3);
+		i3.setSize(Constants.CONTENT, Constants.CONTENT);
+		
 		n3.addGUI(t3);
+		n3.addGUI(i3);
 		this.addScene(n3);
+		
+	//escena 4
 		
 		Narration n4=new Narration();
 		n4.createContents();
 		n4.setTag("INTRO3");
-		Image i4=new Image();
-		i4.setImage("zone");
 		n4.setNext("TOWN");
 		n4.setBackground("COLOR#orange");
+		
 		Text t4 = new Text();
 		t4.setText("Estamos a 4 de Neth, los primeros vientos del invierno empiezan a soplar. " +
 				"Este fue el dia, en 5435, en el que Ekat Kassen y sus hombres marcharon al " +
@@ -146,18 +164,24 @@ public class CryptEverflameAdventure extends Adventure{
 				"por tus compañeros que van apareciendo poco a poco. De pronto la campana de la " +
 				"iglesia empieza a tañir...");
 		t4.setSize(Constants.CONTENT,Constants.CONTENT);
+		
+		Image i4=new Image();
+		i4.setImage("town$S");
 		i4.setPos(Constants.BEGIN, Constants.NEXT);
-		n4.addGUI(i4);
+		i4.setSize(Constants.CONTENT, Constants.CONTENT);
+		
 		n4.addGUI(t4);
+		n4.addGUI(i4);
 		this.addScene(n4);
 
+	//escena 5
+		
 		Narration n5=new Narration();
 		n5.createContents();
 		n5.setBackground("COLOR#orange");
 		n5.setTag("TOWN");
 		n5.setNext("TOWN2");
-		Image i5 = new Image();
-		i5.setImage("town");
+		
 		Text t5 = new Text();
 		t5.setText("La campana sobre el cercano templo de Erastil empieza a tocar su cancion " +
 				"del mediodia, levantando ecos en la silenciosa ciudad de Kassen. Mientras " +
@@ -176,18 +200,23 @@ public class CryptEverflameAdventure extends Adventure{
 				"los bravos que se aventuraran hasta la tumba de kassen para volver con la llama " +
 				"que mantendra a esta comunidad a salvo durante otro invierno?' ");
 		t5.setSize(Constants.CONTENT,Constants.CONTENT);
+		
+		Image i5 = new Image();
+		i5.setImage("town$S");
 		i5.setPos(Constants.BEGIN, Constants.NEXT);
+		i5.setSize(Constants.CONTENT, Constants.CONTENT);
+		
 		n5.addGUI(t5);
 		n5.addGUI(i5);
 		this.addScene(n5);
 		
-		Choice c=new Choice();
+	//escena 6 choice 1
 		
-		c.createContents();
-		c.setBackground("COLOR#white");
-		//c.setBackground("ui/crypteverflame_adventure/town");
-
-		c.setTag("TOWN2");
+		Choice c6=new Choice();
+		c6.createContents();
+		c6.setBackground("COLOR#white");
+		c6.setTag("TOWN2");
+		
 		Text t6 = new Text();
 		t6.setText("El alcalde hace una pausa para que podais aceptar la mision. Uno a uno " +
 				"tus compañeros y tu os acercais,  Uptal os entrega la mula y la lampara. " +
@@ -203,147 +232,205 @@ public class CryptEverflameAdventure extends Adventure{
 				"Los aldeanos os despiden de forma fria, reflejandose la solemnidad del momento en sus " +
 				"rostros. ");
 		t6.setSize(Constants.CONTENT,Constants.CONTENT);
-		c.addGUI(t6);
-		c.addOption("Empezar la aventura!","CHOICE");
-		this.addScene(c);
 		
-		DefaultTileMapScene ms=new DefaultTileMapScene();
-		ms.createContents();
-		ms.createMap(15,15);
-		ms.loadTiled("everflame2", 0, 0);
-		ms.setTiles(DefaultTileMapScene.DEFAULT,"tiles");
-		ms.setProperties(DefaultTileMapScene.DEFAULT, StabBlockData.ID);
-		ms.setTag("EVERFLAME2");
+		Image i6 = new Image();
+		i6.setImage("zone$S");
+		i6.setPos(Constants.BEGIN, Constants.NEXT);
+		i6.setSize(Constants.CONTENT, Constants.CONTENT);
+		
+		c6.addGUI(t6);
+		c6.addOption("Empezar la aventura!","SMOKE");
+		this.addScene(c6);
+		
+	//mapa combate 1
+		
+		DefaultTileMapScene mc1=new DefaultTileMapScene();
+		mc1.createContents();
+		mc1.createMap(15,15);
+		mc1.loadTiled("everflame2", 0, 0);
+		mc1.setTiles(DefaultTileMapScene.DEFAULT,"tiles");
+		mc1.setProperties(DefaultTileMapScene.DEFAULT, StabBlockData.ID);
+		mc1.setTag("EVERFLAME2");
 		
 		AllMonstersDeadRule r=new AllMonstersDeadRule();
 		r.addResponse(new VictoryResponse(0,"VICTORY"));
-		ms.addRule(r);
+		mc1.addRule(r);
 		AllPlayersDeadRule r2=new AllPlayersDeadRule();
 		r2.addResponse(new DefeatResponse(0,"DEFEAT"));
-		ms.addRule(r2);
-		this.addScene(ms);
+		mc1.addRule(r2);
+		this.addScene(mc1);
 		
-		Choice c2=new Choice();
-		c2.createContents();
+	//escena 7 choice 2
 		
-
-		c2.setTag("CHOICE");
-		c2.setBackground("COLOR#white");
-		//c2.setNext("WOLVES");
+		Choice c7=new Choice();
+		c7.createContents();
+		c7.setTag("SMOKE");
+		c7.setBackground("COLOR#yellow");
+		
 		Text t7 = new Text();
 		t7.setText("La dura batalla contra los extraños orcos acaba, dejando una sensacion extraña, por la forma de desaparecer parece " +
 				"claro que estas criaturas no eran mas que ilusiones, pero quien y porque haria algo asi.");
 		t7.setSize(Constants.CONTENT,Constants.CONTENT);
-		c2.addGUI(t7);
-		RolledSkillOptionButton rs = new RolledSkillOptionButton();
-		rs.setText("Investigar la zona.");
-		rs.setSkill(StabConstants.PERCEPTION);
-		rs.setDC(15);
-		rs.setResults("PERCFAIL", "PERCSUCCESS");
-		c2.addOption("Continuar el viaje.", "WOLVES");
-		c2.addOption(rs);
-		this.addScene(c2);
+		
+		Image i7 = new Image();
+		i7.setImage("forest$S");
+		i7.setPos(Constants.BEGIN, Constants.NEXT);
+		i7.setSize(Constants.CONTENT, Constants.CONTENT);
+		
+		RolledSkillOptionButton sb7 = new RolledSkillOptionButton();
+		sb7.setText("Investigar la zona.");
+		sb7.setSkill(StabConstants.PERCEPTION);
+		sb7.setDC(15);
+		sb7.setResults("PERCFAIL", "PERCSUCCESS");
+		c7.addOption(sb7);
+		c7.addOption("Continuar el viaje.", "WOLVES");
+		
+		c7.addGUI(t7);
+		this.addScene(c7);
 	
-		Narration n7=new Narration();
-		n7.createContents();
-		n7.setTag("PERCFAIL");
-		n7.setNext("WOLVES");
-
-		n7.setBackground("COLOR#white");
-		Text t12 = new Text();
-		t7.setText("Buscas por la zona, pero no consigues encontrar nada que te llame la atención. Al cabo de un rato decides continuar con tu camino. ");
-		t7.setSize(Constants.CONTENT,Constants.CONTENT);
-		n7.addGUI(t12);
-		this.addScene(n7);
+	//escena 8
 		
 		Narration n8=new Narration();
 		n8.createContents();
-		n8.setTag("PERCSUCCESS");
+		n8.setTag("PERCFAIL");
 		n8.setNext("WOLVES");
-
 		n8.setBackground("COLOR#white");
-		Text t13 = new Text();
-		t13.setText("Un olor a tabaco de pipa llama poderosamente tu atención. Despues de un rato pensando lo asocias al tabaco que fuma Holgast, el mago de la aldea. " +
-				"Mientras vas meditando sobre ello decides continuar tu viaje.");
-		t13.setSize(Constants.CONTENT,Constants.CONTENT);
-		n8.addGUI(t12);
+		
+		Text t8 = new Text();
+		t8.setText("Buscas por la zona, pero no consigues encontrar nada que te llame la atención. Al cabo de un rato decides continuar con tu camino. ");
+		t8.setSize(Constants.CONTENT,Constants.CONTENT);
+		
+		Image i8 = new Image();
+		i8.setImage("forest$S");
+		i8.setPos(Constants.BEGIN, Constants.NEXT);
+		i8.setSize(Constants.CONTENT, Constants.CONTENT);
+		
+		n8.addGUI(t8);
 		this.addScene(n8);
 		
-	
-		Choice c3=new Choice();
-		c3.createContents();
-
-		c3.setBackground("COLOR#white");
-		c3.setTag("WOLVES");
-		Text t8 = new Text();
-		t8.setText("Despues de derrotar a los orcos seguis avanzando. Gracias al mapa que llevais es relativamente sencillo encontrar el camino correcto. " +
+	//escena 9
+		
+		Narration n9=new Narration();
+		n9.createContents();
+		n9.setTag("PERCSUCCESS");
+		n9.setNext("WOLVES");
+		n9.setBackground("COLOR#white");
+		
+		Text t9 = new Text();
+		t9.setText("Un olor a tabaco de pipa llama poderosamente tu atención. Despues de un rato pensando lo asocias al tabaco que fuma Holgast, el mago de la aldea. " +
+				"Mientras vas meditando sobre ello decides continuar tu viaje.");
+		t9.setSize(Constants.CONTENT,Constants.CONTENT);
+		
+		Image i9 = new Image();
+		i9.setImage("everflame$S");
+		i9.setPos(Constants.BEGIN, Constants.NEXT);
+		i9.setSize(Constants.CONTENT, Constants.CONTENT);
+		
+		n9.addGUI(t9);
+		this.addScene(n9);
+		
+	//escena 10 choice 3
+		
+		Choice c10=new Choice();
+		c10.createContents();
+		c10.setBackground("COLOR#white");
+		c10.setTag("WOLVES");
+		
+		Text t10 = new Text();
+		t10.setText("Despues de derrotar a los orcos seguis avanzando. Gracias al mapa que llevais es relativamente sencillo encontrar el camino correcto. " +
 				"Despues de todo el dia andando queda poco para que oscurezca. Empezais a buscar un sitio para montar las tiendas mientras las sombras de " +
 				"los arboles se alargan mas y mas, cubriendolo todo y dando un aspecto ominoso al bosque antes placido. Con una sensacion de desasosiego " +
 				"empezais a montar el campamento. ");
-		t8.setSize(Constants.CONTENT,Constants.CONTENT);
-		c3.addGUI(t8);
-		c3.addOption("Tirar survival para buscar un buen sitio de acampar", "EVERFLAME1.start");
+		t10.setSize(Constants.CONTENT,Constants.CONTENT);
+	
+		Image i10 = new Image();
+		i10.setImage("forest$S");
+		i10.setPos(Constants.BEGIN, Constants.NEXT);
+		i10.setSize(Constants.CONTENT, Constants.CONTENT);
 		
 		//Ejemplo de rolledSkill
-		RolledSkillOptionButton b= new RolledSkillOptionButton();
-		b.setText("Tirar survival para buscar un buen sitio de acampar");
-		b.setSkill(StabConstants.SURVIVAL);
-		b.setDC(10);
-		b.setResults("escena a la que ir si falla", "escena a la que ir si pasa");
+		RolledSkillOptionButton sb10= new RolledSkillOptionButton();
+		sb10.setText("Tirar survival para buscar un buen sitio de acampar");
+		sb10.setSkill(StabConstants.SURVIVAL);
+		sb10.setDC(10);
+		sb10.setResults("WOLVESSUC", "WOLVESFAIL");
 																					//Si las escenas empiezan por #, eso mueve a todos los jugadores en la escena a la escena indicada
 																					//Esto es aplicable a los casos normales tambien.
 																				    //Ojo que puede que alguien no este en la escena. eso lo vemos mas adelante con scene.setWaitsForAll()
-	//	c3.addOption(b);
 		//Notese  que esto no le pone icono, pero le cambia el tamaño a que ocupe toda la linea
 		//Y esta pendiente de probar, claro :D
 		//RolledOptionButton funciona casi igual
 		
-		c3.addOption("Ayudar a alguien a buscar un sitio", "EVERFLAME1.start");  // Esto va a dar mas guerra, me temo. Deberiamos dejarlo para más adelante.
-		c3.addOption("Acampar en el primer sitio que veas", "EVERFLAME1.start");
-		this.addScene(c3);
+		c10.addGUI(t10);
+		c10.addOption(sb10);  // Esto va a dar mas guerra, me temo. Deberiamos dejarlo para más adelante.
+		c10.addOption("Acampar en el primer sitio que veas", "WOLVESFAIL");
+		c10.addGUI(i10);
+		this.addScene(c10);
 		
-		Choice c4=new Choice();
-		c4.createContents();
-
-		c4.setBackground("COLOR#white");
-		c4.setTag("WOLVES2");
-		Text t9 = new Text();
-		t9.setText("Despues encendeis una pequeña hoguera y empezais a cocinar un estofado con las raciones que hay en " +
+	//escena 11 choice 4
+		
+		Choice c11=new Choice();
+		c11.createContents();
+		c11.setBackground("COLOR#white");
+		c11.setTag("WOLVES2");
+		
+		Text t11 = new Text();
+		t11.setText("Despues encendeis una pequeña hoguera y empezais a cocinar un estofado con las raciones que hay en " +
 	"vuestras mochilas. Mientras cocinais ois a lo lejos unos aullidos. Al cabo de una hora volveis a oir los aullidos pero ahora mucho mas cerca. " +
 	"Pasa otra media hora...");
-		t9.setSize(Constants.CONTENT,Constants.CONTENT);
-		c4.addGUI(t9);
-		c4.addOption("Tirar percepcion!", "EVERFLAME1.start");
-		this.addScene(c4);
+		t11.setSize(Constants.CONTENT,Constants.CONTENT);
 		
-		Choice c5 = new Choice();
-		c5.createContents();
-
-
-		c5.setBackground("COLOR#white");
-		c5.setTag("WOLVES3");
-		Text t10 = new Text();
-		t10.setText("Veis un gran lobo observandoos escondido entre los matorrales. Claramente intenta huir despues de veros, seguramente con la" +
+		Image i11 = new Image();
+		i11.setImage("forest$S");
+		i11.setPos(Constants.BEGIN, Constants.NEXT);
+		i11.setSize(Constants.CONTENT, Constants.CONTENT);
+		
+		c11.addGUI(t11);
+		c11.addOption("Tirar percepcion!", "EVERFLAME1.start");
+		this.addScene(c11);
+		
+	//escena 12 choice 5
+		
+		Choice c12 = new Choice();
+		c12.createContents();
+		c12.setBackground("COLOR#white");
+		c12.setTag("WOLVES3");
+		
+		Text t12 = new Text();
+		t12.setText("Veis un gran lobo observandoos escondido entre los matorrales. Claramente intenta huir despues de veros, seguramente con la " +
 				"intencion de traer al resto de su manada para atacaros. Debeis actuar rapidamente. ");
-		t10.setSize(Constants.CONTENT,Constants.CONTENT);
-		c5.addGUI(t10);
-		c5.addOption("Atacar al lobo", "EVERFLAME1.start");
-		c5.addOption("Tirar wild empathy contra el lobo", "WOLVES4");
-		c5.addOption("Dejar que el lobo huya", "EVERFLAME1.start");
-		this.addScene(c5);
+		t12.setSize(Constants.CONTENT,Constants.CONTENT);
 		
-		Narration n9=new Narration();
-		n9.createContents();
-		n9.setBackground("town");
-		n9.setTag("WOLVES4");
-		n9.setNext("BANDITS");
-		Text t11 = new Text();
-		t11.setText("El lobo se queda escuchandote ladeando la cabeza. Al cabo de un rato se acerca a ti olisqueando tu mano y con un salto " +
+		Image i12 = new Image();
+		i12.setImage("wolf$S");
+		i12.setPos(Constants.BEGIN, Constants.NEXT);
+		i12.setSize(Constants.CONTENT, Constants.CONTENT);
+		
+		c12.addGUI(t12);
+		
+		c12.addOption("Atacar al lobo", "EVERFLAME1.start");
+		c12.addOption("Tirar wild empathy contra el lobo", "WOLVES4");
+		c12.addOption("Dejar que el lobo huya", "EVERFLAME1.start");
+		
+		this.addScene(c12);
+		
+	//escena 13
+		
+		Narration n13=new Narration();
+		n13.createContents();
+		n13.setTag("WOLVES4");
+		n13.setNext("BANDITS");
+		
+		Text t13 = new Text();
+		t13.setText("El lobo se queda escuchandote ladeando la cabeza. Al cabo de un rato se acerca a ti olisqueando tu mano y con un salto " +
 				"desaparece en el bosque. Crees que esta manada no os dara mas problemas, al menos esta noche. El resto de la noche pasa " +
 				"con tranquilidad. ");
-		t11.setSize(Constants.CONTENT,Constants.CONTENT);
-		n9.addGUI(t11);
-		this.addScene(n9);
+		t13.setSize(Constants.CONTENT,Constants.CONTENT);
+		
+		n9.addGUI(t13);
+		this.addScene(n13);
+		
+	//escena derrota
 		
 		Narration ndefeat=new Narration();
 		ndefeat.createContents();
