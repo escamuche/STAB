d46c53690e7f7ec8aface412ca3ae6ce6aa5c95d
Ferrus <malacor@gmail.com>
2013-05-31 12:19:48 +0200

    light

--- stab/src/com/stab/data/actions/player/spells/cleric/level0/Light.java
+++ stab/src/com/stab/data/actions/player/spells/cleric/level0/Light.java
@@ -24,13 +24,14 @@ public class Light extends SpellOnTarget{
 		caster.addTrait(new LightInUse());
 		}
 		
-		Trait light= (LightInUse) caster.getTrait(LightInUse.ID);
-		if(((LightInUse) light).getBuff()!=null){
-				((LightInUse) light).getBuff().end();
-				Light_Buff mylight = new Light_Buff();
-				target.addTrait(mylight);
-				((LightInUse) light).setBuff(mylight);
-			}
+		LightInUse light = (LightInUse) caster.getTrait(LightInUse.ID);
+		if(light.getBuff()!=null)
+				light.getBuff().end();
+		
+		Light_Buff mylight = new Light_Buff();
+		target.addTrait(mylight);
+		light.setBuff(mylight);
+		
 			return super.fullEffect(caster, target, point);
 	}
 	
