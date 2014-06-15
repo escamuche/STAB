f4cd4335adddad01393a6a839a283ef0db8c80d1
Carlos Núñez <escamuche@gmail.com>
2014-06-15 10:55:42 +0200

    ajustes dibujado, soporte para "colorear" tiles en los mapas (ej: dar un tinte verde a ciertas tiles del tileset)

--- stab/src/com/stab/data/utils/StabBlockData.java
+++ stab/src/com/stab/data/utils/StabBlockData.java
@@ -1,5 +1,7 @@
 package com.stab.data.utils;
 
+import org.newdawn.slick.Color;
+
 import com.stab.client.slick.base.map.StabBlockProperties;
 import com.stab.common.map.blocks.BlockData;
 
@@ -8,9 +10,15 @@ public class StabBlockData  extends BlockData {
 	public static final String ID="STAB_BLOCKDATA";
 	
 	public StabBlockData() {
+		
+		
 		 fill(StabBlockProperties.EMPTY,64);
-		 set(StabBlockProperties.SOLID_WALL,2,8);
-		 set(StabBlockProperties.EMPTY_TILE,1,9,17,25);
+		 set(StabBlockProperties.SOLID_WALL,2,8,18,26,34,42,50,58);
+		 set(StabBlockProperties.EMPTY_TILE,1,9,17,25,33,41,49,57);
 		 set(StabBlockProperties.GLASS,16);
+		 
+		 
+		//StabBlockProperties RED_TILE = StabBlockProperties.EMPTY_TILE.copy(Color.red);
+		// set(RED_TILE,1,9,17,25,33,41,49,57);
 	}
 }
