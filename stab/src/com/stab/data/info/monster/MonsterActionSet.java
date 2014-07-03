b220c680f82fa6dfeabb10aafaa7893cf9ac19fb
Carlos Núñez <escamuche@gmail.com>
2014-07-03 15:56:41 +0200

    finalmente, los monstruos han aprendido a usar las armas :D (lease: actualizada la AI para usar el sistema de acciones delegadas)

--- stab/src/com/stab/data/info/monster/MonsterActionSet.java
+++ stab/src/com/stab/data/info/monster/MonsterActionSet.java
@@ -2,12 +2,16 @@ package com.stab.data.info.monster;
 
 import com.stab.data.actions.general.DefendAction;
 import com.stab.data.actions.general.PathfinderWeaponAttackAction;
+import com.stab.data.info.equipment.HumanoidGear;
 import com.stab.model.action.BasicActionSet;
+import com.stab.model.action.base.GearSlotDelegatedAction;
 
 public class MonsterActionSet extends BasicActionSet {
 
 	public MonsterActionSet() {
-		this.addAction(PathfinderWeaponAttackAction.ID);
+		//this.addAction(PathfinderWeaponAttackAction.ID);
+		GearSlotDelegatedAction sl1= new GearSlotDelegatedAction(null, HumanoidGear.MAINHAND, 0);
+		this.addAction(sl1);
 		this.addAction(DefendAction.ID);
 	}
 	
