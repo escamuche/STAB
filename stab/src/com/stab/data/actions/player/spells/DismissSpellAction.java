7cbf44b197f97052671323c69db5e100aa2584af
Carlos Núñez <escamuche@gmail.com>
2014-08-11 15:58:58 +0200

    fix sprites "persistentes". base para dismiss

---
+++ stab/src/com/stab/data/actions/player/spells/DismissSpellAction.java
@@ -0,0 +1,34 @@
+package com.stab.data.actions.player.spells;
+
+import java.awt.Point;
+
+import com.stab.data.info.SpellDecoration;
+import com.stab.data.info.SpellEffect;
+import com.stab.model.action.ContextualAction;
+import com.stab.model.info.Info;
+import com.stab.model.request.basic.ActionRequest;
+
+
+public class DismissSpellAction extends ContextualAction {
+	
+	public static final String ID="DISMISS_SPELL_ACTION";
+	
+	
+	public DismissSpellAction() {
+		setRange(5);
+		setName("Dismiss spell");
+		setResource("actions/spell/dismiss");
+		setDescription("Dismiss the active spell, ending it");
+		setLosType(IN_SIGHT);
+		setTargetInfoClass(SpellDecoration.class);
+	}
+
+
+	@Override
+	public int affect(Info instigator, Info target, Point point,
+			ActionRequest ar) {
+
+		((SpellDecoration)target).getSpellEffect().end();
+		return OK;
+	}
+}
