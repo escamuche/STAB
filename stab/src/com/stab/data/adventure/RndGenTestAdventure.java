b0f57b9c19f56ac8220b0601e575e497891eb86d
Carlos Núñez <escamuche@gmail.com>
2015-01-20 14:52:13 +0100

    compatibilidad random gen con escenas normales, fix click fuera de la zona de mapa, mejoras random gen

--- stab/src/com/stab/data/adventure/RndGenTestAdventure.java
+++ stab/src/com/stab/data/adventure/RndGenTestAdventure.java
@@ -1,10 +1,11 @@
 package com.stab.data.adventure;
 
 import com.stab.adventure.Adventure;
+import com.stab.data.scene.DefaultStabMapScene;
 import com.stab.data.utils.StabBlockData;
 import com.stab.model.basic.scenes.Narration;
 import com.stab.model.basic.scenes.map.DefaultTileMapScene;
-import com.stab.model.basic.scenes.map.gen.RNDTestScene;
+import com.stab.model.basic.scenes.map.gen.BasicMapBuilder;
 
 public class RndGenTestAdventure extends Adventure{
 	
@@ -31,14 +32,19 @@ public class RndGenTestAdventure extends Adventure{
 		n.setText("Generar mapa.");
 		this.addScene(n);
 		
-		RNDTestScene ms=new RNDTestScene();
-		
-		
+		DefaultStabMapScene ms=new DefaultStabMapScene();
+		ms.setTag("BATTLE");
+		ms.createContents();
 		ms.setProperties(DefaultTileMapScene.DEFAULT, StabBlockData.ID);
 		ms.setTiles(DefaultTileMapScene.DEFAULT,"tiles2");
-		ms.setTag("BATTLE");
 	
-		ms.createContents();
+		BasicMapBuilder builder=new BasicMapBuilder();
+		builder.setScene(ms);  //Aqui ya tenemos que tener creado el mapa, y con los tileproperties listos (nos lo han pasado en la aventura)
+		builder.createMap(8, 8);
+		builder.loadParts();     //porque aqui se necesita
+		builder.generateMap();
+		
+		ms.setAllExplored(true);
 		
 		this.addScene(ms);
 		
