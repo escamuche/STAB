e69b9dba8f9416c56e4310feabc26738b27e576b
Carlos Núñez <escamuche@gmail.com>
2015-02-23 22:07:38 +0100

    puertas logicas, sequencers, materiales, y alguna cosa mas

---
+++ stab/src/com/stab/pf/info/trait/material/PfWoodMaterial.java
@@ -0,0 +1,12 @@
+package com.stab.pf.info.trait.material;
+
+import com.stab.model.info.trait.base.Material;
+
+public class PfWoodMaterial extends Material {
+
+	
+	public PfWoodMaterial() {
+		setDensity(0.7f);
+	}
+	
+}
