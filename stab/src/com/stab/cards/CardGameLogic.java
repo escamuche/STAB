49afb0e29a9bff280ad037d95b8edf3df51a814c
Carlos Núñez <escamuche@gmail.com>
2015-03-19 15:57:21 +0100

    tableskillroll, mas cosas de su gamelogic. primeros pasos

--- stab/src/com/stab/cards/CardGameLogic.java
+++ stab/src/com/stab/cards/CardGameLogic.java
@@ -1,7 +1,74 @@
 package com.stab.cards;
 
 import com.stab.adventure.GameLogic;
+import com.stab.model.basic.token.interfaces.Mechanism;
+import com.stab.model.info.BaseInfo;
+import com.stab.model.info.Info;
+import com.stab.model.info.applicable.Applicable;
+import com.stab.model.info.applicable.base.OpposedSkillRoll;
+import com.stab.model.info.applicable.base.SkillRoll;
+import com.stab.model.info.base.Creature;
+import com.stab.model.info.interfaces.ActiveTurnBased;
+import com.stab.pf.StabConstants;
 
 public class CardGameLogic extends GameLogic {
 
+	
+	@Override
+	public SkillRoll getSkillRoll(BaseInfo info, String skill, int dc) {
+		return new TableSkillRoll(info, skill, dc);
+	}
+	
+	@Override
+	public OpposedSkillRoll getOpposedSkillRoll(BaseInfo info, String skill,
+			BaseInfo target, String skill2) {
+		return null;//Ya veremos
+	}
+	
+	@Override
+	public int getInitiativeFor(ActiveTurnBased t) {
+		//Iniciativa agi+int. o 20 fija para objetos y efectos
+		if (t instanceof Creature){
+			int ini=((Creature)t).getValue(CardConstants.AGI);
+			ini+=((Creature)t).getValue(CardConstants.INT);
+			return ini;
+		}
+		return 20;
+	}
+	
+	@Override
+	public void hide(Info i, int check) {
+		//Nada, puesto que ya viene hecho en setHideCheck.
+	}
+	
+	@Override
+	public boolean detectAttempt(Info origin, Info target) {
+		if (!(origin instanceof BaseInfo))
+			return true; //Es algun tipo de efecto especial
+		if (!target.canBeDetected())
+			return false;
+		
+		BaseInfo i=(BaseInfo)origin;
+		SkillRoll sk=getSkillRoll(i, CardConstants.PERCEPTION, target.getHideCheck());
+		sk.check();
+		return sk.success();
+	}
+	
+	@Override
+	public int tinkerAttempt(Info instigator, Mechanism target) {
+		int dc=1;
+			if (!(instigator instanceof BaseInfo))
+				return Applicable.OK; //Es algun tipo de efecto especial
+			
+			BaseInfo i=(BaseInfo)instigator;
+			if (target instanceof Mechanism){
+				dc=((Mechanism)target).getDifficultyCheck();
+			}
+			SkillRoll d= getSkillRoll(i,StabConstants.DISABLEDEVICES,dc);
+			d.check();
+			return d.getResult();
+	}
+	
+	
+	
 }
--- stab/src/com/stab/cards/TableSkillRoll.java
+++ stab/src/com/stab/cards/TableSkillRoll.java
@@ -1,13 +1,59 @@
 package com.stab.cards;
 
+import com.stab.common.utils.Roll;
 import com.stab.model.info.BaseInfo;
 import com.stab.model.info.applicable.base.SkillRoll;
 
 public class TableSkillRoll extends SkillRoll
 {
 
+	static int[] chances={0,10,25,50,75,90,100}; 
+	
+	public static final int IMPOSSIBLE=50;
+	public static final int AUTOMATIC=51;
+	
 	public TableSkillRoll(BaseInfo instigator, String skill, int target) {
 		super(instigator, skill, target);
+		//No es necesario, porque no tiramos normalmente, pero por si acaso lo dejo claro
+		this.cantCrit();
+		setDice(100);
+		setBotchRange(0);
+		setCritRange(0);
+	}
+	
+	public void validate() {
+		if (getResult()==AUTOMATIC_FAIL || getResult()==AUTOMATIC_SUCCESS)
+			return;
+		if (failed())
+			return;
+		int r=getFinalModifier();
+		int t=getFinalTargetNumber();
+		int diff= r-t;
+		if (diff<=-3){
+			setResult(IMPOSSIBLE);
+		}else
+			if (diff>=3){
+				setResult(AUTOMATIC);
+			}else{
+			
+				int roll=Roll.d100();
+				if (roll<=chances[diff+3])
+					setResult(SUCCESS);
+				else
+					setResult(FAIL);
+			}
+			
 	}
 
+	@Override
+	public boolean failed() {
+		if (getResult()==IMPOSSIBLE)
+			return true;
+		if (getResult()==AUTOMATIC)
+			return false;
+		return super.failed();
+	}
+	
+	
+	
 }
